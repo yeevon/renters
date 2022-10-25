f3af6dc28be731b8879e0c7822563ec07328e5df
Jose de Lima <delimajm@gmail.com>
2022-10-25 15:00:38 -0500

    rendered initial html objects

--- src/App.js
+++ src/App.js
@@ -6,51 +6,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div>
+        <label className='inputLabel'>Enter your Zip Code: </label>
+        <input className='zipCodeEntry' id='cusZip' placeholder='12345'></input>
+      </div>
+      <div>
+        <button className='button' id='submitZip'>Submit</button>
+      </div>
+      <div>
+        <label className='yourRent'>YADYADAYADAYADAYADAYADA</label>
+      </div>
     </div>
   );
 }
